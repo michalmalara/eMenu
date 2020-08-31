82efe5b087a71977fdea4997bf3ef3e9087d4f0c
Michal Malara <michalmalara@gmail.com>
2020-08-31 19:00:15 +0200

    Dish model added

--- menu/admin.py
+++ menu/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Menu
+from .models import Menu, Dish
 
 # Register your models here.
 
 admin.site.register(Menu)
+admin.site.register(Dish)
--- menu/models.py
+++ menu/models.py
@@ -1,20 +1,42 @@
 from django.db import models
+from django.db.models import Count
 from django.utils import timezone
 
 
 # Create your models here.
 
+class Dish(models.Model):
+    name = models.TextField(max_length=50, unique=True)
+    description = models.TextField(max_length=1000)
+    price = models.FloatField()
+    preparation_time = models.IntegerField()
+    is_vege = models.BooleanField()
+    created = models.DateTimeField(editable=False)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+        return super(Dish, self).save(*args, **kwargs)
+
+
 class Menu(models.Model):
     name = models.TextField(max_length=50, unique=True)
     description = models.TextField(max_length=1000)
     created = models.DateTimeField(editable=False)
-    modified = models.DateTimeField()
+    modified = models.DateTimeField(auto_now=True)
+    dishes = models.ManyToManyField(Dish)
+
+    def __str__(self):
+        return f'{self.name}: {self.dishes.count()}'
 
     def save(self, *args, **kwargs):
-        """
-            On save, update timestamps
-        """
         if not self.id:
             self.created = timezone.now()
         self.modified = timezone.now()
         return super(Menu, self).save(*args, **kwargs)
+
